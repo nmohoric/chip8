8d6605337c0ec0d9aa7e6110b2518c39faebdba6
Nick Mohoric <nick.mohoric@gmail.com>
2016-04-03 18:00:25 -0400

    Added initCPU to initialize a cpu. Qualified Vector for clarity

--- src/CPU.hs
+++ src/CPU.hs
@@ -3,7 +3,7 @@
 module CPU where
 
 import Data.Word (Word8,Word16)
-import Data.Vector
+import Data.Vector as V
 import Registers
 import Lens.Micro.TH (makeLenses)
 import Lens.Micro ((.~), (%~), (&))
@@ -11,20 +11,33 @@ import Lens.Micro.Mtl (view)
 
 -- http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#2.0
 data CPU = 
-    CPU { _memory     :: Vector Word8
+    CPU { _memory     :: V.Vector Word8
         , _registers  :: Registers 
         , _delayTimer :: Word8
         , _soundTimer :: Word8
         , _i          :: Word16
         , _pc         :: Word16
         , _sp         :: Word8
-        , _stack      :: Vector Word16
-        , _input      :: Vector Bool
-        , _graphics   :: Vector Bool
-         }
+        , _stack      :: V.Vector Word16
+        , _input      :: V.Vector Bool
+        , _graphics   :: V.Vector Bool
+        }
 
 makeLenses ''CPU
 
+initCPU :: CPU
+initCPU = CPU { _memory     = V.replicate 4096 0
+              , _registers  = V.replicate 16 0
+              , _delayTimer = 0
+              , _soundTimer = 0
+              , _i          = 0
+              , _pc         = 0x200
+              , _sp         = 0
+              , _stack      = V.replicate 12 0
+              , _input      = V.replicate 16 False
+              , _graphics   = V.replicate (64 * 32) False
+              }
+              
 increasePC :: CPU -> CPU
 increasePC = pc %~ (+2)
 
